36b28ffa33bb2b8107762459bc98e2decdb174c8
Loïc Vial <contact.coyl@gmail.com>
2015-04-25 19:41:21 +0200

    Plus jolie queue :)

--- include/utils.h
+++ include/utils.h
@@ -2,8 +2,8 @@
 
 #include <qglviewer.h>
 
-using namespace std;
-
 GLuint loadTexture(const char* filename);
 
+void renderCylinder(float x1, float y1, float z1, float x2, float y2, float z2, float radius1, float radius2, int subdivisions, GLUquadricObj* quadric);
 
+void renderCylinder(qglviewer::Vec p1, qglviewer::Vec p2, float radius1, float radius2, int subdivisions, GLUquadricObj* quadric);
--- src/dragontail.cpp
+++ src/dragontail.cpp
@@ -1,5 +1,6 @@
 #include <dragontail.h>
 #include <iostream>
+#include <utils.h>
 
 using namespace std;
 using qglviewer::Vec;
@@ -11,11 +12,11 @@ DragonTail::DragonTail()
 
 void DragonTail::init(Viewer&)
 {
-    particles.push_back(new Sphere(2, Vec(20, 0, 10), Vec(0, 0, 0), 0));
+    particles.push_back(new Sphere(2, Vec(17, 0, 6), Vec(0, 0, 0), 0));
 
     for (int i = 1 ; i < 9 ; i++)
     {
-        particles.push_back(new Sphere(2-(0.2*i), particles[i - 1]->getPosition() + Vec(0, -6, 0), Vec(0, -1, (i%2==0)?(i):(-i)), 1));
+        particles.push_back(new Sphere(2-(0.2*i), particles[i - 1]->getPosition() + Vec(0, -6, 0), Vec(i/2, -1, i/3), 1));
         springs.push_back(new Spring(particles[i],  particles[i-1], 30, 6, 1));
     }
 }
@@ -32,31 +33,19 @@ void DragonTail::draw()
     }
 
     glPopMatrix();
-/*
-    glColor3f(1.0, 0.28, 0.0);
-    glLineWidth(5.0);
-    vector<Spring*>::iterator itS;
-    for (itS = springs.begin(); itS != springs.end(); ++itS)
-    {
-        (*itS)->draw();
-    }
-*/
 
     for (int i = 1 ; i < 9 ; i++)
     {
         glPushMatrix();
 
-        glTranslatef(particles[i-1]->getPosition().x,
-                     particles[i-1]->getPosition().y,
-                     particles[i-1]->getPosition().z);
-        glRotatef(90, 1, 0, 0);
-
-        glColor3f(1, 1, 1); // uniform white -> to be change to texture
+        glColor3f(1, 1, 1); // uniform white -> to be changed to texture
 
         GLUquadric* params = gluNewQuadric();
         gluQuadricDrawStyle(params,GLU_FILL);
+        gluQuadricNormals(params, GLU_SMOOTH);
         gluQuadricTexture(params, GL_FALSE);
-        gluCylinder(params, particles[i-1]->radius, particles[i]->radius, 10, 10, 10);
+        renderCylinder(particles[i-1]->getPosition(), particles[i]->getPosition(),
+                       particles[i-1]->radius, particles[i]->radius, 15, params);
         gluDeleteQuadric(params);
 
         glPopMatrix();
